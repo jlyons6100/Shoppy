5cef65e3d4a9affd6fe5f20f85b55a2e031d3990
James Lyons <jlyons6100@gmail.com>
2017-11-29 00:32:04 -0800

    Added Shopping Item, Database of items, and Shopping Cart as ArrayLists

--- app/src/main/java/com/shoppy/shoppy/Shopping_Item.java
+++ app/src/main/java/com/shoppy/shoppy/Shopping_Item.java
@@ -13,24 +13,31 @@ public class Shopping_Item {
     public String getName(){
         return name;
     }
+
     public String getImage(){
         return image;
     }
+
     public double getPrice(){
         return price;
     }
+    
     public String getDescription(){
         return description;
     }
+
     public void setName(String name){
         this.name = name;
     }
+
     public void setImage(String image){
         this.image = image;
     }
+
     public void setPrice(double price){
         this.price = price;
     }
+
     public void setDescription(String description){
         this.description = description;
     }

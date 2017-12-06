7e8e65cc819a96feba62bfb77c1c6b0cac53d14d
Hao Wang <definiter@gmail.com>
2017-12-06 01:38:45 -0800

    Add shopping cart UI.

--- app/src/main/java/com/shoppy/shoppy/MainActivity.java
+++ app/src/main/java/com/shoppy/shoppy/MainActivity.java
@@ -59,6 +59,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+//                .setDefaultFontPath("fonts/Roboto-RobotoRegular.ttf")
+//                .setFontAttrId(R.attr.fontPath)
+//                .build()
+//        );
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);

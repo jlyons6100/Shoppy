532416f085d954c3afefcdd57252c66c8157e5ba
yolandawww <yolanda.wang@stanford.edu>
2017-12-07 00:49:35 -0800

    shopping cart half

--- app/src/main/java/com/shoppy/shoppy/CartActivity.java
+++ app/src/main/java/com/shoppy/shoppy/CartActivity.java
@@ -60,92 +60,108 @@ public class CartActivity extends AppCompatActivity {
         startActivityForResult(intent, 0);
     }
 
-
-
     public void drawCart(final ArrayList<Shopping_Item> cart)
     {
-        if (cart.size() == 0){
-            LinearLayout linear_scrollview_horizontal = findViewById(R.id.cartpage_linear_scrollview);
-            linear_scrollview_horizontal.setVisibility(View.VISIBLE);
-        }
-//        else {
-//            LinearLayout linear_scrollview_horizontal = findViewById(R.id.cartpage_linear_scrollview);
-//            linear_scrollview_horizontal.setVisibility(View.GONE);
-//
-//            LinearLayout linear_scrollview = (LinearLayout) findViewById(R.id.cartpage_linear_scrollview);
-//
-//            LinearLayout one_item_temp = findViewById(R.id.one_item);
-//            for (int i = 0 ; i < cart.size(); i++) {
-//                LinearLayout card = new LinearLayout(getApplicationContext());
-//                card.setLayoutParams(one_item_temp.getLayoutParams());
-//                card.setOrientation(LinearLayout.HORIZONTAL);
-//                card.setGravity(Gravity.BOTTOM);
-//
-//                // Item image
-//                ImageView item_img = new ImageView(getApplicationContext());
-//                ImageView item_img_temp = findViewById(R.id.item_image);
-//                item_img.setLayoutParams(item_img_temp.getLayoutParams());
-//                Context c = getApplicationContext();
-//                int id = c.getResources().getIdentifier("drawable/" + cart.get(i).getImage(), null, c.getPackageName());
-//                item_img.setImageResource(id);
-//                card.addView(item_img);
-//
-//                // Item text
-//                RelativeLayout item_text_box = new RelativeLayout(getApplicationContext());
-//                RelativeLayout item_text_box_temp = findViewById(R.id.item_text_box);
-//                item_text_box.setLayoutParams(item_text_box_temp.getLayoutParams());
-//                item_text_box.setBackgroundResource(R.drawable.rounded_corner);
-//
-//                // Item text -- name
-//                TextView item_name = new TextView(getApplicationContext());
-//                item_name.setTextAppearance(R.style.item_name);
-//                item_name.setText(cart.get(i).getName());
-//                item_text_box.addView(item_name);
-//
-//                // Item text -- detail
-//                TextView item_detail = new TextView(getApplicationContext());
-//                TextView detail_temp = findViewById(R.id.item_detail);
-//                item_detail.setLayoutParams(detail_temp.getLayoutParams());
-//                item_detail.setTextAppearance(R.style.item_detail);
-//                item_detail.setText(cart.get(i).getDescription());
-//                item_text_box.addView(item_detail);
-//
-//                // Item text -- price and add
-//                RelativeLayout cart_box = new RelativeLayout(getApplicationContext()); //add to item text box
-//                RelativeLayout cart_box_temp = findViewById(R.id.cart_box);
-//                cart_box.setLayoutParams(cart_box_temp.getLayoutParams());
-//                cart_box.setBackgroundResource(R.drawable.rounded_corner);
-//
-//                TextView carts_text1 = new TextView(getApplicationContext());
-//                TextView dollar_temp = findViewById(R.id.cart_dollar);
-//                carts_text1.setLayoutParams(dollar_temp.getLayoutParams());
-//                carts_text1.setTextAppearance(R.style.item_dollar);
-//                carts_text1.setText("$");
-//                TextView carts_text2 = new TextView(getApplicationContext());
-//                TextView price_temp = findViewById(R.id.cart_price);
-//                carts_text2.setLayoutParams(price_temp.getLayoutParams());
-//                carts_text2.setTextAppearance(R.style.item_price);
-//                carts_text2.setText(""+cart.get(i).getPrice());
-//                TextView carts_text3 = new TextView(getApplicationContext());
-//                TextView wet_temp = findViewById(R.id.cart_wet);
-//                carts_text3.setLayoutParams(wet_temp.getLayoutParams());
-//                carts_text3.setTextAppearance(R.style.item_wet);
-//                carts_text3.setText(" / 1L");
-//                cart_box.addView(carts_text1);
-//                cart_box.addView(carts_text2);
-//                cart_box.addView(carts_text3);
-//
-//                RelativeLayout buttonAdd = new RelativeLayout(getApplicationContext());
-//                RelativeLayout buttonAdd_temp = findViewById(R.id.button_add);
-//                buttonAdd.setClickable(true);
-//
-//                buttonAdd.setLayoutParams(buttonAdd_temp.getLayoutParams());
-//                buttonAdd.setPadding(buttonAdd_temp.getPaddingLeft(), buttonAdd_temp.getPaddingTop(), buttonAdd_temp.getPaddingRight(), buttonAdd_temp.getPaddingBottom());
-//                buttonAdd.setBackgroundResource(R.drawable.add_button);
-//                buttonAdd.setId(i);
-//
-//                final TextView num = new TextView(getApplicationContext());
-//                num.setText( " " + cart.get(i).getAmount());
+
+        LinearLayout linear_scrollview_horizontal = findViewById(R.id.cartpage_linear_scrollview);
+
+        LinearLayout one_item_temp = findViewById(R.id.one_item);
+        ImageView item_img_temp = findViewById(R.id.item_image);
+        RelativeLayout item_text_box_temp = findViewById(R.id.item_text_box);
+        TextView name_temp = findViewById(R.id.item_name);
+        TextView detail_temp = findViewById(R.id.item_detail);
+        RelativeLayout cart_box_temp = findViewById(R.id.cart_box);
+
+        TextView dollar_temp = findViewById(R.id.cart_dollar);
+        TextView price_temp = findViewById(R.id.cart_price);
+        TextView wet_temp = findViewById(R.id.cart_wet);
+
+        for (int i = 0 ; i < cart.size(); i++) {
+            LinearLayout card = new LinearLayout(getApplicationContext());
+            card.setLayoutParams(one_item_temp.getLayoutParams());
+            card.setOrientation(LinearLayout.HORIZONTAL);
+            card.setGravity(Gravity.BOTTOM);
+
+            // Item image
+            ImageView item_img = new ImageView(getApplicationContext());
+            item_img.setLayoutParams(item_img_temp.getLayoutParams());
+            Context c = getApplicationContext();
+            int id = c.getResources().getIdentifier("drawable/" + cart.get(i).getImage(), null, c.getPackageName());
+            item_img.setImageResource(id);
+            card.addView(item_img);
+
+            // Item text
+            RelativeLayout item_text_box = new RelativeLayout(getApplicationContext());
+            item_text_box.setLayoutParams(item_text_box_temp.getLayoutParams());
+            item_text_box.setBackgroundResource(R.drawable.rounded_corner);
+
+            // Item text -- name
+            TextView item_name = new TextView(getApplicationContext());
+            item_name.setLayoutParams(name_temp.getLayoutParams());
+            item_name.setTextAppearance(R.style.item_name);
+            item_name.setText(cart.get(i).getName());
+            item_text_box.addView(item_name);
+
+            // Item text -- detail
+            TextView item_detail = new TextView(getApplicationContext());
+            item_detail.setLayoutParams(detail_temp.getLayoutParams());
+            item_detail.setTextAppearance(R.style.item_detail);
+            item_detail.setText(cart.get(i).getDescription());
+            item_text_box.addView(item_detail);
+
+            // Item text -- price
+            RelativeLayout cart_box = new RelativeLayout(getApplicationContext()); //add to item text box
+            cart_box.setLayoutParams(cart_box_temp.getLayoutParams());
+            cart_box.setBackgroundResource(R.drawable.rounded_corner);
+
+            TextView carts_text1 = new TextView(getApplicationContext());
+            carts_text1.setLayoutParams(dollar_temp.getLayoutParams());
+            carts_text1.setTextAppearance(R.style.item_dollar);
+            carts_text1.setText("$");
+            TextView carts_text2 = new TextView(getApplicationContext());
+            carts_text2.setLayoutParams(price_temp.getLayoutParams());
+            carts_text2.setTextAppearance(R.style.item_price);
+            carts_text2.setText(""+cart.get(i).getPrice());
+            TextView carts_text3 = new TextView(getApplicationContext());
+            carts_text3.setLayoutParams(wet_temp.getLayoutParams());
+            carts_text3.setTextAppearance(R.style.item_wet);
+            carts_text3.setText(" / 1L");
+            cart_box.addView(carts_text1);
+            cart_box.addView(carts_text2);
+            cart_box.addView(carts_text3);
+            item_text_box.addView(cart_box);
+
+            card.addView(item_text_box);
+
+            RelativeLayout amount_bar = new RelativeLayout(getApplicationContext());
+            RelativeLayout amount_bar_temp = findViewById(R.id.amount_bar);
+            amount_bar.setLayoutParams(amount_bar_temp.getLayoutParams());
+
+
+
+            ImageView minus = new ImageView(getApplicationContext());
+            ImageView minus_temp = findViewById(R.id.amount_minus);
+            minus.setLayoutParams(minus_temp.getLayoutParams());
+            minus.setImageResource(R.drawable.ic_remove_circle_black_24dp);
+            amount_bar.addView(minus);
+
+            final TextView num = new TextView(getApplicationContext());
+            TextView amount_num_temp = findViewById(R.id.amount_num);
+//            textTemplate(amount_num, amount_num_temp);
+            num.setLayoutParams(amount_num_temp.getLayoutParams());
+            num.setText("" + cart.get(i).getAmount());
+            num.setGravity(Gravity.CENTER);
+            amount_bar.addView(num);
+
+            ImageView plus = new ImageView(getApplicationContext());
+            ImageView plus_temp = findViewById(R.id.amount_plus);
+            plus.setLayoutParams(plus_temp.getLayoutParams());
+            plus.setImageResource(R.drawable.ic_add_circle_black_24dp);
+            amount_bar.addView(plus);
+
+            minus.setClickable(true);
+            plus.setClickable(true);
+
 //
 //                ImageView minus = new ImageView(getApplicationContext());
 //                int width_minus = (int)convertDpToPixel(30, getApplicationContext());
@@ -153,31 +169,31 @@ public class CartActivity extends AppCompatActivity {
 //                LinearLayout.LayoutParams paramsminus = new LinearLayout.LayoutParams(width_minus, height_minus);
 //                minus.setLayoutParams(paramsminus);
 //                minus.setImageResource(R.mipmap.ic_minus_round);
-//                minus.setId(i);
-//                minus.setOnClickListener(new View.OnClickListener() {
-//                    public void onClick(View v) {
-//                        //v.getId() will give you the image id
-//                        if (cart.get(v.getId()).getAmount() != 0) {
-//                            cart.get(v.getId()).setAmount(cart.get(v.getId()).getAmount() - 1);
-//                            num.setText(" " + cart.get(v.getId()).getAmount());
-//                            TextView price_text = (TextView) findViewById(R.id.cart_price);
-//                            double total_price = 0;
-//                            int amount_of_items = 0;
-//                            for (int i = 0; i < cart.size(); i++){
-//                                amount_of_items += cart.get(i).getAmount();
-//                                total_price+= (cart.get(i).getAmount()*cart.get(i).getPrice());
-//                            }
-//                            price_text.setText("$"+total_price+" / "+amount_of_items+ " items");
-//                        }
-//                        else {
-//                            Context context = getApplicationContext();
-//                            CharSequence text = "You can't buy a negative amount of an item!";
-//                            int duration = Toast.LENGTH_SHORT;
-//                            Toast toast = Toast.makeText(context, text, duration);
-//                            toast.show();
-//                        }
-//                    }
-//                });
+                minus.setId(i);
+                minus.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        //v.getId() will give you the image id
+                        if (cart.get(v.getId()).getAmount() != 0) {
+                            cart.get(v.getId()).setAmount(cart.get(v.getId()).getAmount() - 1);
+                            num.setText("" + cart.get(v.getId()).getAmount());
+                            TextView price_text = (TextView) findViewById(R.id.cart_price);
+                            double total_price = 0;
+                            int amount_of_items = 0;
+                            for (int i = 0; i < cart.size(); i++){
+                                amount_of_items += cart.get(i).getAmount();
+                                total_price+= (cart.get(i).getAmount()*cart.get(i).getPrice());
+                            }
+                            price_text.setText("$"+total_price+" / "+amount_of_items+ " items");
+                        }
+                        else {
+                            Context context = getApplicationContext();
+                            CharSequence text = "You can't buy a negative amount of an item!";
+                            int duration = Toast.LENGTH_SHORT;
+                            Toast toast = Toast.makeText(context, text, duration);
+                            toast.show();
+                        }
+                    }
+                });
 //                card.addView(minus);
 //
 //
@@ -188,34 +204,40 @@ public class CartActivity extends AppCompatActivity {
 //                LinearLayout.LayoutParams paramsplus = new LinearLayout.LayoutParams( width_minus, height_minus);
 //                plus.setLayoutParams(paramsplus);
 //                plus.setImageResource(R.mipmap.ic_plus_round);
-//                plus.setId(i);
-//                plus.setOnClickListener(new View.OnClickListener() {
-//                    public void onClick(View v) {
-//                        //v.getId() will give you the image id
-//                        cart.get(v.getId()).setAmount(cart.get(v.getId()).getAmount() +1);
-//                        num.setText( " " +cart.get(v.getId()).getAmount());
-//                        TextView price_text = (TextView) findViewById(R.id.cart_price);
-//                        double total_price = 0;
-//                        int amount_of_items = 0;
-//                        for (int i = 0; i < cart.size(); i++){
-//                            amount_of_items += cart.get(i).getAmount();
-//                            total_price+= (cart.get(i).getAmount()*cart.get(i).getPrice());
-//                        }
-//                        price_text.setText("$"+total_price+" / "+amount_of_items+ " items");
-//                    }
-//                });
-//                card.addView(plus);
+                plus.setId(i);
+                plus.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        //v.getId() will give you the image id
+                        cart.get(v.getId()).setAmount(cart.get(v.getId()).getAmount() +1);
+                        num.setText( "" +cart.get(v.getId()).getAmount());
+                        TextView price_text = (TextView) findViewById(R.id.cart_price);
+                        double total_price = 0;
+                        int amount_of_items = 0;
+                        for (int i = 0; i < cart.size(); i++){
+                            amount_of_items += cart.get(i).getAmount();
+                            total_price+= (cart.get(i).getAmount()*cart.get(i).getPrice());
+                        }
+                        price_text.setText("$"+total_price+" / "+amount_of_items+ " items");
+                    }
+                });
 //                if (card != null)
-//                linear_scrollview.addView(card);
-//            }
-//            TextView price_text = (TextView) findViewById(R.id.cart_price);
-//            double total_price = 0;
-//            int amount_of_items = 0;
-//            for (int i = 0; i < cart.size(); i++){
-//               amount_of_items += cart.get(i).getAmount();
-//               total_price+= (cart.get(i).getPrice()*cart.get(i).getAmount());
-//            }
-//            price_text.setText("$"+total_price+" / "+amount_of_items + " items");
+            card.addView(amount_bar);
+
+            linear_scrollview_horizontal.addView(card);
+            View v2 = new View(getApplicationContext());
+            View v2_temp = findViewById(R.id.card_line);
+            v2.setLayoutParams(v2_temp.getLayoutParams());
+            v2.setBackground(v2_temp.getBackground());
+            card.addView(v2);
+        }
+//        TextView price_text = (TextView) findViewById(R.id.cart_price);
+//        double total_price = 0;
+//        int amount_of_items = 0;
+//        for (int i = 0; i < cart.size(); i++){
+//           amount_of_items += cart.get(i).getAmount();
+//           total_price+= (cart.get(i).getPrice()*cart.get(i).getAmount());
 //        }
+//        price_text.setText("$"+total_price+" / "+amount_of_items + " items");
+
     }
 }
